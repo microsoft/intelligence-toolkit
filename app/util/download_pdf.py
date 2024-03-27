a7dea47f814e6c644d5505bb8341365c8f7c9067
Dayenne Souza <ddesouza@microsoft.com>
2024-03-27 14:43:38 -0300

    fix download_pdf

--- app/util/download_pdf.py
+++ app/util/download_pdf.py
@@ -12,18 +12,15 @@ css = '''body {
 text_label = 'Report generated using Intelligence Toolkit (https://aka.ms/itk)'
 
 def add_download_pdf(name, text, button_text='Download PDF', is_markdown=True, disabled=False):
-    # Ensure the file name ends with '.pdf'
     if not name.endswith('.pdf'):
         name += '.pdf'
-    text = f'{text}<hr>{text_label}'
+    text = f'<style>{css}</style>{text}<hr>{text_label}'
     # Convert text to HTML if it's in Markdown format
     text = markdown2.markdown(text) if is_markdown else text
 
     # Generate PDF from HTML string
     config_pdf = config_pdfkit()
     with tempfile.NamedTemporaryFile(suffix='.pdf', delete=False) as temp_file:
-        text = f'<style>{css}</style>' + text
-
         pdfkit.from_string(text, temp_file.name, options=pdfkit_options, configuration=config_pdf)
         
         # Provide download button for the generated PDF
