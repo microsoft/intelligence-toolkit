5a24fedaff5c4ac7f74ebf6d87077e020f0e6986
Darren Edge <darren.edge@example.com>
2024-03-27 09:28:38 +0000

    Documentation typo fixes

--- app/workflows/record_matching/config.py
+++ app/workflows/record_matching/config.py
@@ -17,7 +17,7 @@ The **Record Matching** workflow generates intelligence reports on record matche
 1. [**Input**] Multiple datasets representing overlapping entities with inconsistent record formats/naming conventions. 
 2. [**Embedding Calls**] The records are harmonized and embedded into a multi-dimensional semantic space, with similar records close to one another.
 3. [**Process**] Clusters of matching records are converted into groups, with user-controllable similarity thresholds.
-5. [**Ouput**] Record groups CSV file containing all records matched to the same group. Can be created and used independently without any AI or embedding calls.
+5. [**Output**] Record groups CSV file containing all records matched to the same group. Can be created and used independently without any AI or embedding calls.
 6. [**AI Calls**] On request from the user, the system uses generative AI to evaluate the likelihood of the records representing a real-world match.
 7. [**Output**] AI match report CSV file evaluating the likelihood of a real-world match for each of identified record groups.
 
