b675b13c5154ed6d03dfbcd859c2cc863b08aa56
Darren Edge <darren.edge@example.com>
2024-04-17 21:11:28 +0100

    Compound value splitting

--- app/util/ui_components.py
+++ app/util/ui_components.py
@@ -204,6 +204,8 @@ def prepare_input_df(workflow, input_df_var, processed_df_var, output_df_var, id
         st.session_state[f'{workflow}_selected_num_bins'] = 5
     if f'{workflow}_selected_trim_percent' not in st.session_state:
         st.session_state[f'{workflow}_selected_trim_percent'] = 0.05
+    if f'{workflow}_selected_compound_cols' not in st.session_state:
+        st.session_state[f'{workflow}_selected_compound_cols'] = []
 
     with st.expander('Set subject identifier', expanded=False):
 
@@ -246,9 +248,21 @@ def prepare_input_df(workflow, input_df_var, processed_df_var, output_df_var, id
     selected_cols = [col for col in input_df_var.value.columns.values if st.session_state[f'{workflow}_{col}'] == True]
     processed_df_var.value = processed_df_var.value[['Subject ID']].copy()
     for col in selected_cols:
+        
         if col in st.session_state[f'{workflow}_binned_df'].columns.values:
-            processed_df_var.value[col] = st.session_state[f'{workflow}_binned_df'][col]
-    
+            processed_df_var.value[col] = list(st.session_state[f'{workflow}_binned_df'][col])
+            processed_df_var.value[col] = processed_df_var.value[col].replace('nan', '')
+
+    for (cols, delim) in st.session_state[f'{workflow}_selected_compound_cols']:
+        for col in cols:
+            if col in selected_cols:
+                # add each value as a separate column with a 1 if the value is present in the compound column and None otherwise
+                values = processed_df_var.value[col].apply(lambda x: [y.strip() for y in x.split(delim)] if type(x) == str else [])
+                unique_values = set([v for vals in values for v in vals])
+                for val in unique_values:
+                    processed_df_var.value[col] [col+'_'+val] = values.apply(lambda x: 1 if val in x and val != 'nan' else None)
+                processed_df_var.value.drop(columns=[col], inplace=True)
+
     if selected_cols != st.session_state[f'{workflow}_last_attributes']:
         processed_df_var.value = util.df_functions.fix_null_ints(processed_df_var.value)
         st.session_state[f'{workflow}_last_attributes'] = selected_cols
@@ -387,23 +401,17 @@ def prepare_input_df(workflow, input_df_var, processed_df_var, output_df_var, id
             st.session_state[f'{workflow}_last_attributes'] = [] # hack to force second rerun and show any changes from binning
             st.rerun() 
 
+
+
     with st.expander('Expand compound values', expanded=False):
         options = [x for x in processed_df_var.value.columns.values if x != 'Subject ID']
         selected_compound_cols = st.multiselect('Select compound columns to expand', options, help='Select the columns you want to expand into separate columns. If you do not select any columns, no expansion will be performed.')
         col_delimiter = st.text_input('Column delimiter', value='', help='The character used to separate values in compound columns. If the delimiter is not present in a cell, the cell will be left unchanged.')
         if st.button('Expand selected columns', key='expand_compound'):
-            bdf = st.session_state[f'{workflow}_binned_df']
-            for col in selected_compound_cols:
-                if col_delimiter != '':
-                    # add each value as a separate column with a 1 if the value is present in the compound column and None otherwise
-                    values = processed_df_var.value[col].apply(lambda x: [y.strip() for y in x.split(col_delimiter)] if type(x) == str else [])
-                    unique_values = set([v for vals in values for v in vals])
-                    for val in unique_values:
-                        bdf[col+'_'+val] = values.apply(lambda x: 1 if val in x else None)
-                        processed_df_var.value[col+'_'+val] = bdf[col+'_'+val]
-                    bdf.drop(columns=[col], inplace=True)
-                    processed_df_var.value.drop(columns=[col], inplace=True)
-            st.rerun()
+            to_add = (selected_compound_cols, col_delimiter)
+            if col_delimiter != '' and to_add not in st.session_state[f'{workflow}_selected_compound_cols']:
+                st.session_state[f'{workflow}_selected_compound_cols'].append(to_add)
+                st.rerun() 
 
 
     with st.expander('Suppress insignificant attribute values', expanded=False):
