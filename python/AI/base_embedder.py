b60bb17e74204b13a4d52e710511602425a137ff
Dayenne Souza <ddesouza@microsoft.com>
2024-08-22 16:52:09 -0300

    add retry decorator for lancedb operation (#25)
    * add retry decotator
    
    * fix decorator
    
    * fix exception
    
    * add best practices
    
    * fix types
    
    * return any
    
    * fix imports

--- python/AI/base_embedder.py
+++ python/AI/base_embedder.py
@@ -11,6 +11,7 @@ import pyarrow as pa
 from python.AI.defaults import DEFAULT_LLM_MAX_TOKENS
 from python.AI.vector_store import VectorStore
 from python.helpers.constants import CACHE_PATH
+from python.helpers.decorators import retry_with_backoff
 
 from .utils import get_token_count, hash_text
 
@@ -38,6 +39,7 @@ class BaseEmbedder(ABC):
         self.vector_store = VectorStore(db_name, db_path, schema)
         self.max_tokens = max_tokens
 
+    @retry_with_backoff()
     def embed_store_one(self, text: str, cache_data=True) -> Any | list[float]:
         text_hashed = hash_text(text)
         existing_embedding = (
@@ -62,6 +64,7 @@ class BaseEmbedder(ABC):
             raise Exception(msg)
         return embedding
 
+    @retry_with_backoff()
     def embed_store_many(
         self, texts: list[str], callback=None, cache_data=True
     ) -> np.ndarray[Any, np.dtype[Any]]:
