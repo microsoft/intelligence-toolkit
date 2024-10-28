c97528c0aa01df89fa354f17c4912bf2cfb00dc8
Dayenne Souza <ddesouza@microsoft.com>
2024-10-28 14:31:31 -0300

    remove expand component and include more file encoding options

--- app/util/constants.py
+++ app/util/constants.py
@@ -8,3 +8,16 @@ PDF_ENCODING = "UTF-8"
 PDF_WKHTMLTOPDF_PATH = "C:\\Program Files\\wkhtmltopdf\\bin\\wkhtmltopdf.exe"
 MAX_SIZE_EMBEDDINGS_KEY = "max_embedding"
 LOCAL_EMBEDDING_MODEL_KEY = "local_embedding_model"
+
+
+FILE_ENCODING_OPTIONS = [
+    "unicode-escape",
+    "utf-8",
+    "utf-8-sig",
+    "Windows-1252",
+    "ISO-8859-1",
+    "ASCII",
+    "Big5",
+    "Shift JIS",
+]
+FILE_ENCODING_DEFAULT = "unicode-escape"
