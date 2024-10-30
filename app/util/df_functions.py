2e7cf26722ad81aaddd183487ba68c50bdb04d77
Dayenne Souza <ddesouza@microsoft.com>
2024-10-30 10:17:12 -0300

    fix_null_int

--- app/util/df_functions.py
+++ app/util/df_functions.py
@@ -1,31 +1,19 @@
 # Copyright (c) 2024 Microsoft Corporation. All rights reserved.
 import math
 import re
-import sys
 
-import numpy as np
 import pandas as pd
 from dateutil import parser as dateparser
 
 
-def fix_null_ints(in_df):
+def fix_null_ints(in_df: pd.DataFrame) -> pd.DataFrame:
     df = in_df.copy()
     for col, dt in zip(df.columns, df.dtypes, strict=False):
         if dt == "float64":
-            idf = df[[col]].copy()
-            idf["float"] = [x if not np.isnan(x) else 0 for x in idf[col]]
-            idf["int"] = [int(x) if not np.isnan(x) else 0 for x in idf[col]]
-            idf["float_s"] = [x if not np.isnan(x) else -sys.maxsize for x in idf[col]]
-            idf["int_s"] = [
-                int(x) if not np.isnan(x) else -sys.maxsize for x in idf[col]
-            ]
-            fsum = idf["float"].sum()
-            isum = idf["int"].sum()
-            if int(fsum) == int(isum):
-                df[col] = idf["int_s"]
-                df[col] = df[col].astype(str)
-                df[col] = df[col].replace(-sys.maxsize, np.nan)
-    return df
+            df[col] = df[col].astype("Int64")
+            df[col] = df[col].where(pd.notna(df[col]), pd.NA)
+
+    return df.astype(str).replace("<NA>", "")
 
 
 def get_current_time():
