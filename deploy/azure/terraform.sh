e0a0372bb0619d056124ad78c949ca3f7683f320
Dayenne Souza <ddesouza@microsoft.com>
2024-12-09 14:44:26 -0300

    fix terraform script

--- deploy/azure/terraform.sh
+++ deploy/azure/terraform.sh
@@ -4,9 +4,9 @@
 if ! command -v terraform &> /dev/null
 then
     echo "Terraform not found. Installing..."
-    curl -sLo terraform.zip https://releases.hashicorp.com/terraform/1.10.0/terraform_1.10.0_linux_amd64.zip
+    wget  https://releases.hashicorp.com/terraform/1.10.0/terraform_1.10.0_linux_amd64.zip -O terraform.zip
     unzip terraform.zip
-    sudo mv terraform /usr/local/bin/
+    mv terraform /usr/local/bin/
     rm terraform.zip
 else
     echo "Terraform is already installed."
@@ -17,10 +17,10 @@ terraform --version
 
 # Download Terraform configuration files
 echo "Downloading Terraform configuration files..."
-curl -o ./main.tf https://raw.githubusercontent.com/microsoft/intelligence-toolkit/refs/heads/terraform/deploy/azure/main.tf
-curl -o ./variables.tf https://raw.githubusercontent.com/microsoft/intelligence-toolkit/refs/heads/terraform/deploy/azure/variables.tf
-curl -o ./modules/auth/main.tf https://raw.githubusercontent.com/microsoft/intelligence-toolkit/refs/heads/terraform/deploy/azure/modules/auth/main.tf
-curl -o ./modules/auth/variables.tf https://raw.githubusercontent.com/microsoft/intelligence-toolkit/refs/heads/terraform/deploy/azure/modules/auth/variables.tf
+wget https://raw.githubusercontent.com/microsoft/intelligence-toolkit/refs/heads/terraform/deploy/azure/main.tf -O ./main.tf
+wget https://raw.githubusercontent.com/microsoft/intelligence-toolkit/refs/heads/terraform/deploy/azure/variables.tf -O ./variables.tf 
+wget https://raw.githubusercontent.com/microsoft/intelligence-toolkit/refs/heads/terraform/deploy/azure/modules/auth/main.tf -O ./modules/auth/main.tf
+wget https://raw.githubusercontent.com/microsoft/intelligence-toolkit/refs/heads/terraform/deploy/azure/modules/auth/variables.tf -O ./modules/auth/variables.tf 
 
 # Initialize Terraform
 terraform init
