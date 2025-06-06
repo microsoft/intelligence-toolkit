cecc99ed947a63ecec737beb55a48c1d18df8ee5
Dayenne Souza <ddesouza@microsoft.com>
2025-06-06 16:16:51 -0300

    null is not working so I removed it

--- intelligence_toolkit/extract_record_data/prompts.py
+++ intelligence_toolkit/extract_record_data/prompts.py
@@ -3,7 +3,7 @@ You are a helpful assistant tasked with extracting a structured JSON object from
 
 You should generate a new object that adheres to the schema and contains all relevant information from the input text.
 
-Do not fabricate information that is not present in the input text. If information is not present, the value should be NULL
+Do not fabricate information that is not present in the input text.
 
 --Generation guidance--
 
